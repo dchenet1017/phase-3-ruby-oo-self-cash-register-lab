9728fd2491db040a3197e4dcff43d542e8ee31b5
Dimitri Chenet <dchenet1017@gmail.com>
2021-11-03 10:58:02 -0400

    commit tree 037e6c3e619e97764cd7500b0248a15ae09694e2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
 
+require "pry"
+
+class CashRegister
+ attr_accessor :total, :discount, :title, :price, :quantity
+
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title, price, quantity=1)
+        
+        @total += price * quantity
+
+    end
+
+
+    def apply_discount 
+        @total * (@discount /100)
+        # binding.pry
+        
+    end 
+
+
+
+    def cash_register
+        
+    end
+
+    def cash_register_with_discount
+       
+    end
+
+end
